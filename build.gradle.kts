6f9b66dd91dfd500628e86f06eb33fcb89bfc18e
Open592 Developer <uss@open592.com>
2024-11-18 00:00:26 -0800

    gradle: Make gradle easier to use
    Signed-off-by: Open592 Developer <uss@open592.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
-buildscript {
-    repositories {
-        jcenter()
-    }
-    dependencies {
-        classpath(kotlin("gradle-plugin", version = "2.0.21"))
-    }
-}
-
 plugins {
+    base
+    application
     kotlin("jvm") version "2.0.21"
 }
 
-group = "world.gregs.rs2.file"
+application {
+    mainClass.set("world.gregs.rs2.file.Main")
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+group = "world.gregs"
 group = "1.0.0"
 
 repositories {
@@ -30,5 +31,11 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.9.0")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.11.3")
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testImplementation("io.mockk:mockk:1.13.13")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
